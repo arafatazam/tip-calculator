5a95757a409cb34c78c29ddec5c0eac8715fc623
Arafat Azam <arafatazam17@gmail.com>
2023-06-07 20:59:59 +0100

    React hook form added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { useForm } from "react-hook-form";
+
 function App() {
+
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+
+  console.log(watch("example")); // watch input value by passing the name of it
 
   return (
     <>
      <h1 className="text-3xl font-bold underline">Tip Calculator!</h1>
+     <form onSubmit={handleSubmit(onSubmit)}>
+        <input defaultValue="test" {...register("example")} />
+        <input {...register("exampleRequired", { required: true })} />
+        {errors.exampleRequired && <span>This field is required</span>}
+        <input type="submit" />
+      </form>
     </>
   )
 }
